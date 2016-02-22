3726a31ed113d44e2a3426e54d7f45a6a0f69d4c
Tran Tuan <tranit1209@gmail.com>
2016-02-22 15:06:20 +0700

    Init new template

--- application/templates/home/index.php
+++ application/templates/home/index.php
@@ -1,1 +1,209 @@
-<?php echo $test ?>
+<!-- top tiles -->
+<div class="row tile_count">
+<div class="animated flipInY col-md-2 col-sm-4 col-xs-4 tile_stats_count">
+    <div class="left"></div>
+    <div class="right">
+        <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
+        <div class="count">2500</div>
+        <span class="count_bottom"><i class="green">4% </i> From last Week</span>
+    </div>
+</div>
+<div class="animated flipInY col-md-2 col-sm-4 col-xs-4 tile_stats_count">
+    <div class="left"></div>
+    <div class="right">
+        <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
+        <div class="count">123.50</div>
+        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>
+    </div>
+</div>
+<div class="animated flipInY col-md-2 col-sm-4 col-xs-4 tile_stats_count">
+    <div class="left"></div>
+    <div class="right">
+        <span class="count_top"><i class="fa fa-user"></i> Total Males</span>
+        <div class="count green">2,500</div>
+        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+    </div>
+</div>
+<div class="animated flipInY col-md-2 col-sm-4 col-xs-4 tile_stats_count">
+    <div class="left"></div>
+    <div class="right">
+        <span class="count_top"><i class="fa fa-user"></i> Total Females</span>
+        <div class="count">4,567</div>
+        <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last Week</span>
+    </div>
+</div>
+<div class="animated flipInY col-md-2 col-sm-4 col-xs-4 tile_stats_count">
+    <div class="left"></div>
+    <div class="right">
+        <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>
+        <div class="count">2,315</div>
+        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+    </div>
+</div>
+<div class="animated flipInY col-md-2 col-sm-4 col-xs-4 tile_stats_count">
+    <div class="left"></div>
+    <div class="right">
+        <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
+        <div class="count">7,325</div>
+        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+    </div>
+</div>
+
+</div>
+<!-- /top tiles -->
+<table class="table table-striped responsive-utilities jambo_table bulk_action">
+                                        <thead>
+                                            <tr class="headings">
+                                                <th>
+                                                    <input type="checkbox" id="check-all" class="flat">
+                                                </th>
+                                                <th class="column-title">Invoice </th>
+                                                <th class="column-title">Invoice Date </th>
+                                                <th class="column-title">Order </th>
+                                                <th class="column-title">Bill to Name </th>
+                                                <th class="column-title">Status </th>
+                                                <th class="column-title">Amount </th>
+                                                <th class="column-title no-link last"><span class="nobr">Action</span>
+                                                </th>
+                                                <th class="bulk-actions" colspan="7">
+                                                    <a class="antoo" style="color:#fff; font-weight:500;">Bulk Actions ( <span class="action-cnt"> </span> ) <i class="fa fa-chevron-down"></i></a>
+                                            </th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr class="even pointer">
+                                    <td class="a-center "><input type="checkbox" class="flat" name="table_records" ></td>
+                                    <td class=" ">121000040</td>
+                                    <td class=" ">May 23, 2014 11:47:56 PM </td>
+                                    <td class=" ">121000210 <i class="success fa fa-long-arrow-up"></i></td>
+                                    <td class=" ">John Blank L</td>
+                                    <td class=" ">Paid</td>
+                                    <td class="a-right a-right ">$7.45</td>
+                                    <td class=" last"><a href="#">View</a>
+                                                    </td>
+                                            </tr>
+                                            <tr class="odd pointer">
+                                                <td class="a-center ">
+                                                    <input type="checkbox" class="flat" name="table_records">
+                                                </td>
+                                                <td class=" ">121000039</td>
+                                                <td class=" ">May 23, 2014 11:30:12 PM</td>
+                                                <td class=" ">121000208 <i class="success fa fa-long-arrow-up"></i>
+                                                </td>
+                                                <td class=" ">John Blank L</td>
+                                                <td class=" ">Paid</td>
+                                                <td class="a-right a-right ">$741.20</td>
+                                                <td class=" last"><a href="#">View</a>
+                                                </td>
+                                            </tr>
+                                            <tr class="even pointer">
+                                                <td class="a-center ">
+                                                    <input type="checkbox" class="flat" name="table_records">
+                                                </td>
+                                                <td class=" ">121000038</td>
+                                                <td class=" ">May 24, 2014 10:55:33 PM</td>
+                                                <td class=" ">121000203 <i class="success fa fa-long-arrow-up"></i>
+                                                </td>
+                                                <td class=" ">Mike Smith</td>
+                                                <td class=" ">Paid</td>
+                                                <td class="a-right a-right ">$432.26</td>
+                                                <td class=" last"><a href="#">View</a>
+                                                </td>
+                                            </tr>
+                                            <tr class="odd pointer">
+                                                <td class="a-center ">
+                                                    <input type="checkbox" class="flat" name="table_records">
+                                                </td>
+                                                <td class=" ">121000037</td>
+                                                <td class=" ">May 24, 2014 10:52:44 PM</td>
+                                                <td class=" ">121000204</td>
+                                                <td class=" ">Mike Smith</td>
+                                                <td class=" ">Paid</td>
+                                                <td class="a-right a-right ">$333.21</td>
+                                                <td class=" last"><a href="#">View</a>
+                                                </td>
+                                            </tr>
+                                            <tr class="even pointer">
+                                                <td class="a-center ">
+                                                    <input type="checkbox" class="flat" name="table_records">
+                                                </td>
+                                                <td class=" ">121000040</td>
+                                                <td class=" ">May 24, 2014 11:47:56 PM </td>
+                                                <td class=" ">121000210</td>
+                                                <td class=" ">John Blank L</td>
+                                                <td class=" ">Paid</td>
+                                                <td class="a-right a-right ">$7.45</td>
+                                                <td class=" last"><a href="#">View</a>
+                                                </td>
+                                            </tr>
+                                            <tr class="odd pointer">
+                                                <td class="a-center ">
+                                                    <input type="checkbox" class="flat" name="table_records">
+                                                </td>
+                                                <td class=" ">121000039</td>
+                                                <td class=" ">May 26, 2014 11:30:12 PM</td>
+                                                <td class=" ">121000208 <i class="error fa fa-long-arrow-down"></i>
+                                                </td>
+                                                <td class=" ">John Blank L</td>
+                                                <td class=" ">Paid</td>
+                                                <td class="a-right a-right ">$741.20</td>
+                                                <td class=" last"><a href="#">View</a>
+                                                </td>
+                                            </tr>
+                                            <tr class="even pointer">
+                                                <td class="a-center ">
+                                                    <input type="checkbox" class="flat" name="table_records">
+                                                </td>
+                                                <td class=" ">121000038</td>
+                                                <td class=" ">May 26, 2014 10:55:33 PM</td>
+                                                <td class=" ">121000203</td>
+                                                <td class=" ">Mike Smith</td>
+                                                <td class=" ">Paid</td>
+                                                <td class="a-right a-right ">$432.26</td>
+                                                <td class=" last"><a href="#">View</a>
+                                                </td>
+                                            </tr>
+                                            <tr class="odd pointer">
+                                                <td class="a-center ">
+                                                    <input type="checkbox" class="flat" name="table_records">
+                                                </td>
+                                                <td class=" ">121000037</td>
+                                                <td class=" ">May 26, 2014 10:52:44 PM</td>
+                                                <td class=" ">121000204</td>
+                                                <td class=" ">Mike Smith</td>
+                                                <td class=" ">Paid</td>
+                                                <td class="a-right a-right ">$333.21</td>
+                                                <td class=" last"><a href="#">View</a>
+                                                </td>
+                                            </tr>
+
+                                            <tr class="even pointer">
+                                                <td class="a-center ">
+                                                    <input type="checkbox" class="flat" name="table_records">
+                                                </td>
+                                                <td class=" ">121000040</td>
+                                                <td class=" ">May 27, 2014 11:47:56 PM </td>
+                                                <td class=" ">121000210</td>
+                                                <td class=" ">John Blank L</td>
+                                                <td class=" ">Paid</td>
+                                                <td class="a-right a-right ">$7.45</td>
+                                                <td class=" last"><a href="#">View</a>
+                                                </td>
+                                            </tr>
+                                            <tr class="odd pointer">
+                                                <td class="a-center ">
+                                                    <input type="checkbox" class="flat" name="table_records">
+                                                </td>
+                                                <td class=" ">121000039</td>
+                                                <td class=" ">May 28, 2014 11:30:12 PM</td>
+                                                <td class=" ">121000208</td>
+                                                <td class=" ">John Blank L</td>
+                                                <td class=" ">Paid</td>
+                                                <td class="a-right a-right ">$741.20</td>
+                                                <td class=" last"><a href="#">View</a>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+
+                                    </table>
